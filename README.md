5482120409f64a7b8ab08dda6150e7e940ae89d6
karlomijaljevic <karlo.mijaljevic@gmail.com>
2025-07-20 16:14:40 +0200

    removed forced formatting to flac, and simplified script, gplv3 used now

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# YTDW
+# YTDW - YouTube Audio Download
 
 This simple script is used to download audio from YouTube. It needs three
 programs to works properly:
@@ -7,14 +7,41 @@ programs to works properly:
 - [ffmpeg](https://ffmpeg.org/)
 - [aria2](https://github.com/aria2/aria2)
 
-It can download playlists as well. It accepts two parameters. The
-first parameter is the URL to the video/playlist and the second parameter, which
-is an optional one is the video/directory name.
+It can download playlists as well. It accepts two parameters. The first
+parameter is the URL to the video/playlist and the second parameter, which is
+an optional one is the video/directory name.
 
 It will save audio either into a directory defined by the environment variable
-`AUDIO_DIRECTORY` and if none is found it will save it into `$HOME/Downloads`.
+`AUDIO_DIRECTORY` and if none is found it will save it into `$HOME/music`.
 
-The GPL-2.0 license is added to respect aria2 and ffmpeg.
+The audio will be saved in the `opus` format, which is a modern audio codec
+used by YouTube. If you want to change the format, you can do so by editing
+the `ffmpeg` command in the script. For example, if you want to save the audio
+in `mp3` format, you can change the command to (starts on line 91):
+
+```sh
+ yt-dlp --quiet \
+    --no-warnings \
+    --progress \
+    --ignore-errors \
+    --no-mtime \
+    --downloader aria2c \
+    --downloader-args "\
+      --user-agent='$g_user_agent' \
+      --max-connection-per-server=16 \
+      --split=16 \
+    " \
+    --extract-audio \
+    --prefer-ffmpeg \
+    --audio-format mp3 \
+    --audio-quality 0 \
+    --postprocessor-args "-q:a 0 -map a" \
+    -o "$name" \
+    "$url"
+```
+
+As you can see, the `--audio-format` option is set to `mp3` which will
+convert the audio to `mp3` format instead of `opus`.
 
 ## Example use case
 
--- ytdw.sh
+++ ytdw.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 #
 # Copyright (C) 2025 Karlo Mijaljević
-#
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -12,9 +11,16 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ==================== DESCRIPTION OF PROGRAM ====================
+#
+# Use this script to download audio from YouTube. It can either download a
+# single audio file or a playlist. The audio will be downloaded in OPUS format
+# and saved in the specified directory. If no directory is specified, it will
+# be saved in the default directory which is set to $HOME/music. The script
+# uses yt-dlp for downloading and aria2c for downloading the audio files.
 #
 
 # =========================== #
@@ -22,7 +28,8 @@
 # =========================== #
 
 # User defined flies and directories
-g_audio_dir="${AUDIO_DIRECTORY:-"$HOME/Downloads"}"
+g_url="$1"
+g_audio_dir="${AUDIO_DIRECTORY:-"$HOME/music"}"
 g_temp_dir="/tmp/ytdw"
 
 # Control variables used between multiple functions
@@ -45,147 +52,126 @@ g_user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/1
 # Checks for the program dependencies, which are: aria2c, yt-dlp and ffmpeg.
 # And creates the temporary directory in $g_temp_dir
 function f_check_dependencies {
-        if command -v aria2c > /dev/null 2>&1 ; then
-                true
-        else
-                echo -e "${c_red}Program 'aria2' not found!$c_normal"
-                exit 1
-        fi
+   if ! command -v aria2c &> /dev/null; then
+    echo -e "${c_red}Program 'aria2' not found!$c_normal"
+    exit 1
+  fi
 
-        if command -v yt-dlp > /dev/null 2>&1 ; then
-                true
-        else
-                echo -e "${c_red}Program 'yt-dlp' not found!$c_normal"
-                exit 2
-        fi
+  if ! command -v yt-dlp &> /dev/null; then
+    echo -e "${c_red}Program 'yt-dlp' not found!$c_normal"
+    exit 1
+  fi
 
-        if command -v ffmpeg > /dev/null 2>&1 ; then
-                true
-        else
-                echo -e "${c_red}Program 'ffmpeg' not found!$c_normal"
-                exit 3
-        fi
+  if ! command -v ffmpeg &> /dev/null; then
+    echo -e "${c_red}Program 'ffmpeg' not found!$c_normal"
+    exit 1
+  fi
 
-        if [ ! -d "$g_temp_dir" ]; then
-                mkdir -p "$g_temp_dir"
-        fi
+  if [ ! -d "$g_temp_dir" ]; then
+    mkdir -p "$g_temp_dir"
+  fi
 }
 
 # Downloads audio. Takes two parameters. First is the audio URL and seconds is
 # the directory where the audio will be located. The "return" of this function
 # is the return code of the yt-dlp program call.
 function f_dw_audio {
-        local url="$1"
-        local dir="$2"
-        local name_command=""
+  local url="$1"
+  local dir="$2"
+  local name=""
 
-        if [ $g_is_playlist -eq 1 ] || [ -z "$g_dir_or_audio_name" ]; then
-                name_command="$dir/%(title)s.%(ext)s"
-        else
-                name_command="$dir/$g_dir_or_audio_name.%(ext)s"
-        fi
+  if [ $g_is_playlist -eq 1 ] || [ -z "$g_dir_or_audio_name" ]; then
+    name="$dir/%(title)s.%(ext)s"
+  else
+    name="$dir/$g_dir_or_audio_name.%(ext)s"
+  fi
 
+  echo "Downloading video from URL '$url'"
 
-        yt-dlp --quiet \
-                --no-warnings \
-                --progress \
-                --ignore-errors \
-                --no-mtime \
-                --downloader aria2c \
-                --downloader-args "\
-                        --user-agent='$g_user_agent' \
-                        --max-connection-per-server=16 \
-                        --split=16 \
-                " \
-                --extract-audio \
-                --prefer-ffmpeg \
-                --audio-format flac \
-                --audio-quality 0 \
-                --postprocessor-args "-q:a 0 -map a" \
-                -o "$name_command" \
-                "$url"
-}
-
-# Downloads audio. Takes two parameters. First is the audio URL and seconds is
-# the directory where the audio will be located. Once the audio is downloaded
-# or fails to download it will echo the result to the user.
-function f_dw_audio_and_notify {
-        local url="$1"
-        local dir="$2"
-
-        echo "Downloading video from URL '$url'"
-
-        if ! f_dw_audio "$url" "$dir"; then
-                echo -e "${c_red}FAILED to download video from URL '$url'!$c_normal"
-        else
-                echo -e "${c_green}Video from URL '$url' downloaded!$c_normal"
-        fi
+  if ! yt-dlp --quiet \
+    --no-warnings \
+    --progress \
+    --ignore-errors \
+    --no-mtime \
+    --downloader aria2c \
+    --downloader-args "\
+      --user-agent='$g_user_agent' \
+      --max-connection-per-server=16 \
+      --split=16 \
+    " \
+    --extract-audio \
+    --prefer-ffmpeg \
+    --audio-quality 0 \
+    --postprocessor-args "-q:a 0 -map a" \
+    -o "$name" \
+    "$url"; then
+    echo -e "${c_red}Failed to download audio from URL '$url'!$c_normal"
+  else
+    echo -e "${c_green}Audio from URL '$url' downloaded successfully!$c_normal"
+  fi
 }
 
 # Parses the command line parameter which can either be a playlist or a single
 # audio from YT.
 function f_parse_data_and_dw {
-        local url="$1"
-        local dir="$g_audio_dir"
-        local start_time=0
-        local end_time=0
-        local runtime=0
+  local dir="$g_audio_dir"
+  local start_time=0
+  local end_time=0
+  local runtime=0
 
-        start_time="$(date +%s)"
+  start_time="$(date +%s)"
 
-        if [[ "$url" == *"playlist"* ]]; then
-                echo "Starting to download playlist at URL: $url"
-                
-                g_is_playlist=1
+  if [[ "$g_url" == *"playlist"* ]]; then
+    echo "Starting to download playlist at URL: $g_url"
 
-                if [ -n "$g_dir_or_audio_name" ]; then
-                        dir="$g_audio_dir/$g_dir_or_audio_name"
-                else
-                        dir="$g_audio_dir/$(date '+%H%M%S%d%m')-playlist"
-                fi
+    g_is_playlist=1
 
-                if [ ! -d "$dir" ]; then
-                        mkdir -p "$dir"
-                fi
-                
-                g_temp_file="${g_temp_dir}/$(date '+%H%M%S%d%m')-playlist.txt"
+    if [ -n "$g_dir_or_audio_name" ]; then
+      dir="$g_audio_dir/$g_dir_or_audio_name"
+    else
+      dir="$g_audio_dir/$(date '+%H%M%S%d%m')-playlist"
+    fi
 
-                yt-dlp --quiet \
-                        --no-warnings \
-                        --flat-playlist \
-                        --ignore-errors \
-                        --print-to-file url \
-                        "$g_temp_file" "$url"
+    if [ ! -d "$dir" ]; then
+      mkdir -p "$dir"
+    fi
 
+    g_temp_file="${g_temp_dir}/$(date '+%H%M%S%d%m')-playlist.txt"
 
-                readarray -t urls < "$g_temp_file"
+    yt-dlp --quiet \
+      --no-warnings \
+      --flat-playlist \
+      --ignore-errors \
+      --print-to-file url \
+      "$g_temp_file" "$g_url"
 
-                for url in "${urls[@]}"; do
-                        f_dw_audio_and_notify "$url" "$dir"
-                        echo
-                done
+    readarray -t urls < "$g_temp_file"
 
-                rm "$g_temp_file"
-        else
-                f_dw_audio_and_notify "$url" "$dir"
-        fi
+    for url in "${urls[@]}"; do
+      f_dw_audio "$url" "$dir"
+      echo
+    done
 
-        end_time="$(date +%s)"
-        runtime=$((end_time-start_time))
-        echo
-        echo "Total program runtime lasted for $runtime seconds!"
-        echo
-        exit 0
-}
+    rm "$g_temp_file"
+  else
+    f_dw_audio "$g_url" "$dir"
+  fi
 
-# The main function. Starts the program.
-function f_main {
-        f_check_dependencies
-        f_parse_data_and_dw "$1"
+  end_time="$(date +%s)"
+  runtime=$((end_time-start_time))
+  echo
+  echo "Total program runtime lasted for $runtime seconds!"
+  echo
+  exit 0
 }
 
 # =========================== #
 # =========== MAIN ========== #
 # =========================== #
 
-f_main "$1"
+function f_main {
+  f_check_dependencies
+  f_parse_data_and_dw
+}
+
+f_main
